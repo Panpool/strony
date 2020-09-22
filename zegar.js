226a041be57a59cf17f90b6fdb868bc9128cbbca
Panpool <71608721+Panpool@users.noreply.github.com>
2020-09-22 13:27:05 +0200

    Create zegar.js

---
+++ zegar.js
@@ -0,0 +1,26 @@
+div.zegar {
+  color: red;
+  text-align: center;
+  font-size: 60px;
+  margin-top: 0px;
+  font-family: 'bebas neue', cursive;
+  animation-name: zegar;
+  animation-duration: 20s;
+  animation-iteration-count: infinite;
+
+}
+@keyframes zegar {
+  0% {color: #ff0000;}
+  10% {color: #fc7b03;}
+  20% {color: #fcfc03;}
+  30% {color: #73fc03;}
+  40% {color: #03fcd7;}
+  50% {color: #0324fc;}
+  60% {color: #8003fc;}
+  70% {color: #a903fc;}
+  80% {color: #fc03f8;}
+  90% {color: #fc0352;}
+  100% {color: #ff0000;}
+  
+
+}
