21fb87ad7ea4b33eeff5c3d8a0a91a7211d97457
Panpool <71608721+Panpool@users.noreply.github.com>
2020-09-24 16:46:58 +0200

    Create ukryj.js

---
+++ ukryj.js
@@ -0,0 +1,8 @@
+function myFunction(){  
+  var x = document.getElementById("ukryte");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "visible";
+  }
+}
